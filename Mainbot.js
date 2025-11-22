461b147372337ac4a4637aaed71525aa3d022116
magicz2084 <zahragharazi12084magic@gmail.com>
2025-11-22 09:33:44 +0100

    Update Mainbot.js

--- Mainbot.js
+++ Mainbot.js
@@ -1,4 +1,4 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('✨𝓜𝓪𝓰𝓲𝓬✨');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
@@ -20,5 +20,21 @@ client.on('message', msg => {
 });
 
 client.initialize();
+client.on('message', async msg => {
+    // Ping Command
+    if (msg.body === '.ping') {
+        msg.reply('pong');
+    }
+
+    // YouTube MP4 Download
+    if (msg.body.startsWith('.ytmp4 ')) {
+        const url = msg.body.split(' ')[1];
+        require('./lib/ytmp4.js')(msg, url);
+    }
+});
+
+client.initialize();
+
+
 
 
